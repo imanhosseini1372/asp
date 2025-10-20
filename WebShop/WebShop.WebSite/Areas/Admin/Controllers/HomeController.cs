b48871d53f24551360a43c6bc767332063370fb5
imanhosseini1372 <iman.hosseini1372@gmail.com>
2025-10-20 11:31:24 +0330

    CreateProject

---
+++ WebShop/WebShop.WebSite/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.WebSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+
+        [Route("Admin/Dashboard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
